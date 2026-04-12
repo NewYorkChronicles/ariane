c697ea8d6f91392ee2067de7d4d050a20d9b017d
Dryxio <momemtumcorpfzco@gmail.com>
2026-04-12 14:00:55 +0100

    more fix

--- tools/euryopa/euryopa.cpp
+++ tools/euryopa/euryopa.cpp
@@ -1618,10 +1618,6 @@ LoadGame(void)
 	AddColSlot("generic");
 	AddIplSlot("generic");
 
-	AddCdImage("MODELS\\GTA3.IMG");
-	if(isSA())
-		AddCdImage("MODELS\\GTA_INT.IMG");
-
 	if(ModloaderIsActive()){
 		ModloaderDatEntry imgEntries[64];
 		int nImg = ModloaderGetAdditions(imgEntries, 64);
